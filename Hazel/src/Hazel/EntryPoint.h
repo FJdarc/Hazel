32c995438914e1e163b8ace39effebd4a694d620
FJdarc <1204551784@qq.com>
2025-03-20 08:01:13 +0800

    更新 CMake 配置，添加 VSCode 任务和设置，优化构建流程，增加调试信息

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,7 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+    printf("Hazel Engine\n");
     auto app = Hazel::CreateApplication();
     app->Run();
     delete app;
