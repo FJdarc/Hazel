05bcf858a441912432f0231cbf3f35fe541bfbc5
FJdarc <1204551784@qq.com>
2025-05-28 23:21:16 +0800

    close window event

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Core.h"
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
@@ -12,8 +11,8 @@ namespace Hazel
     public:
         static void Init();
 
-        static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-        static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+        inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+        inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
         
     private:
         static std::shared_ptr<spdlog::logger> s_CoreLogger;
