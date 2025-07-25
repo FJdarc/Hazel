8c4fc43c8136940a9418ceed61ce05acf23842a7
FJdarc <1204551784@qq.com>
2025-07-25 13:56:06 +0800

    feat(core): 添加应用程序框架和入口点
    - 新增 Application 类，包含基本的应用程序生命周期方法
    - 创建 Core.h 文件，定义 HAZEL_API 宏用于 DLL 导出/导入
    - 实现 EntryPoint.h 文件，提供应用程序的主入口点
    - 添加 Sandbox 示例应用程序

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Core.h"
+
+namespace Hazel
+{
+    class HAZEL_API Application
+    {
+    public:
+        Application();
+        virtual ~Application();
+
+        void Run();
+    };
+
+    // To be defined in CLIENT
+    Application* CreateApplication();
+}
