f358c5874346d6c424b1ff8e80b46db7bcb39513
FJdarc <1204551784@qq.com>
2025-07-28 19:56:01 +0800

    refactor(log): 重构日志系统
    - 移除了 Log 类中的静态成员变量和 GetCoreLogger/GetClientLogger 方法
    - 在 Log.h 中定义了新的宏，直接使用 spdlog::get 获取 logger
    - 简化了 Log::Init 方法，直接在头文件中实现
    - 删除了 Log.cpp 中的冗余代码

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -9,31 +9,23 @@ namespace Hazel
     class HAZEL_API Log
     {
     public:
-        static void Init();
-
-        inline static std::shared_ptr<spdlog::logger>& GetCoreLogger()
+        static void Init()
         {
-            return s_CoreLogger;
+            spdlog::set_pattern("%^[%T] %n: %v%$");
+            spdlog::stdout_color_mt("HAZEL");
+            spdlog::stdout_color_mt("APP");
         }
-
-        inline static std::shared_ptr<spdlog::logger>& GetClientLogger()
-        {
-            return s_ClientLogger;
-        }
-
-    private:
-        static std::shared_ptr<spdlog::logger> s_CoreLogger;
-        static std::shared_ptr<spdlog::logger> s_ClientLogger;
     };
-} // namespace Hazel
+}
 
-#define HZ_CORE_TRACE(...)    ::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define HZ_CORE_INFO(...)     ::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define HZ_CORE_WARN(...)     ::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define HZ_CORE_ERROR(...)    ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define HZ_CORE_FATAL(...)    ::Hazel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
-#define HZ_TRACE(...)         ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define HZ_INFO(...)          ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
-#define HZ_WARN(...)          ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define HZ_ERROR(...)         ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
-#define HZ_FATAL(...)         ::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define HZ_CORE_TRACE(...)  spdlog::get("HAZEL")->trace(__VA_ARGS__)
+#define HZ_CORE_INFO(...)  spdlog::get("HAZEL")->info(__VA_ARGS__)
+#define HZ_CORE_WARN(...)  spdlog::get("HAZEL")->warn(__VA_ARGS__)
+#define HZ_CORE_ERROR(...)  spdlog::get("HAZEL")->error(__VA_ARGS__)
+#define HZ_CORE_CRITICAL(...)  spdlog::get("HAZEL")->critical(__VA_ARGS__)
+
+#define HZ_TRACE(...)  spdlog::get("APP")->trace(__VA_ARGS__)
+#define HZ_INFO(...)  spdlog::get("APP")->info(__VA_ARGS__)
+#define HZ_WARN(...)  spdlog::get("APP")->warn(__VA_ARGS__)
+#define HZ_ERROR(...)  spdlog::get("APP")->error(__VA_ARGS__)
+#define HZ_CRITICAL(...)  spdlog::get("APP")->critical(__VA_ARGS__)
